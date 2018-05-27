6e4010afb791ddba8415601a3537ffb9e1c5e9d1
Wessie <github@wessie.info>
2018-05-27 21:57:55 +0200

    streamer: remove more graceful code that was leftover

--- cmd/streamer/main.go
+++ cmd/streamer/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"flag"
 	"fmt"
-	"net"
 	"os"
 	"os/signal"
 
@@ -16,58 +15,33 @@ import (
 func main() {
 	configPath := flag.String("conf", "hanyuu.toml",
 		"filepath to configuration file")
-	graceful := flag.Bool("graceful", false, "internal use only")
-
 	flag.Parse()
 
 	s, err := streamer.NewState(*configPath)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("startup:", err)
 		return
 	}
 
-	// we're expecting files from an old process
-	if *graceful {
-		fmt.Println("graceful: attempting graceful restart")
-		file := os.NewFile(3, "listener")
-		l, err := net.FileListener(file)
-		if err != nil {
-			fmt.Println("graceful: new: failed to inherit socket:", err)
-		}
-		if err = file.Close(); err != nil {
-			fmt.Println("graceful: new: failed closing inherited socket:", err)
-		}
+	signalCh := make(chan os.Signal, 2)
+	errCh := make(chan error, 2)
 
-		file = os.NewFile(4, "icecast")
-		conn, err := net.FileConn(file)
-		if err != nil {
-			fmt.Println("graceful: new: failed to inherit conn:", err)
-		}
-		if err = file.Close(); err != nil {
-			fmt.Println("graceful: new: failed closing inherited conn:", err)
-		}
-
-		s.GracefulSetup(l, conn)
-		// only start the streamer right away if we're restarting ourselves
-		s.StartStreamer()
-	} else {
-		// nothing to graceful with, but we have to setup the mechanisms to
-		// avoid issues so pass nils
-		s.GracefulSetup(nil, nil)
-	}
-
-	fmt.Println(s.Conf())
 	go func() {
 		err := streamer.ListenAndServe(s)
 		if err != nil {
 			fmt.Println("http: serve error:", err)
+			errCh <- err
 		}
 	}()
 
-	ch := make(chan os.Signal, 2)
-	signal.Notify(ch, os.Interrupt)
+	signal.Notify(signalCh, os.Interrupt)
 
-	<-ch
-	fmt.Println("received signal, exiting process")
+	select {
+	case <-signalCh:
+		fmt.Println("received interrupt signal, exiting process")
+	case <-errCh:
+		fmt.Println("received error, exiting process")
+	}
+
 	s.Shutdown()
 }
