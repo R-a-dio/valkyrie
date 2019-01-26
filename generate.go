d8d0d72821b5085bc286ff8dd804c74800c976ea
Wessie <github@wessie.info>
2019-01-26 16:10:19 +0100

    rpc: redone .proto files with WKTs

--- generate.go
+++ generate.go
@@ -1,3 +1,3 @@
 package radio
 
-//go:generate protoc --twirp_out=paths=source_relative:. --go_out=paths=source_relative:. rpc/types.proto rpc/streamer.proto rpc/manager.proto rpc/ircbot.proto
+//go:generate protoc --twirp_out=paths=source_relative:. --go_out=paths=source_relative:. rpc/streamer.proto rpc/manager.proto rpc/announcer.proto
