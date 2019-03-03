d901112bf977619975fd78bada477942b143b5ea
Wessie <github@wessie.info>
2019-03-03 16:55:47 +0100

    mariadb: added implementations of UserStorage and RequestStorage
    also add required StorageService interface and internal types to support
    transactions

---
+++ database/mariadb/user.go
@@ -0,0 +1,57 @@
+package mariadb
+
+import (
+	"log"
+	"regexp"
+
+	radio "github.com/R-a-dio/valkyrie"
+	"github.com/R-a-dio/valkyrie/errors"
+	"github.com/jmoiron/sqlx"
+)
+
+// UserStorage implements radio.UserStorage
+type UserStorage struct {
+	handle handle
+}
+
+// LookupName implements radio.UserStorage
+func (us UserStorage) LookupName(name string) (*radio.User, error) {
+	const op errors.Op = "mariadb/UserStorage.LookupName"
+
+	var query = `
+	SELECT
+		IFNULL(users.user, '') AS username,	
+		djs.id AS 'dj.id',
+		djs.regex AS 'dj.regex'
+	FROM
+		djs
+	LEFT JOIN
+		users ON djs.id = users.djid;
+	`
+
+	var users []radio.User
+
+	err := sqlx.Select(us.handle, &users, query)
+	if err != nil {
+		return nil, errors.E(op, err)
+	}
+
+	for _, user := range users {
+		if user.DJ.Regex == "" {
+			// skip users with no regex
+			continue
+		}
+
+		re, err := regexp.Compile(`(?i)` + user.DJ.Regex)
+		if err != nil {
+			log.Printf("%s: invalid regex field: %v", op, err)
+			continue
+		}
+
+		if re.MatchString(name) {
+			return &user, nil
+		}
+	}
+
+	return nil, errors.E(op, errors.UserUnknown, errors.Info(name))
+}
