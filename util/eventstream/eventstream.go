4045e34f1fa7821d5ea38dfe79b1868267ca2725
Wessie <github@wessie.info>
2024-01-19 15:29:07 +0000

    util/eventstream: add stream pubsub package

---
+++ util/eventstream/eventstream.go
@@ -0,0 +1,183 @@
+package eventstream
+
+import (
+	"context"
+	"io"
+	"log"
+	"sync/atomic"
+	"time"
+)
+
+const (
+	SUBSCRIBE cmd = iota
+	SEND
+	LEAVE
+	SHUTDOWN
+	INIT
+)
+
+const TIMEOUT = time.Millisecond * 100
+
+type cmd byte
+
+type request[T any] struct {
+	cmd cmd
+	ch  chan T
+	m   T
+}
+
+func NewEventStream[M any](initial M) *EventStream[M] {
+	es := &EventStream[M]{
+		shutdownCh: make(chan struct{}),
+		reqs:       make(chan request[M]),
+		subs:       make([]chan M, 0, 16),
+		last:       atomic.Pointer[M]{},
+	}
+
+	es.last.Store(&initial)
+	go es.run()
+
+	return es
+}
+
+type EventStream[M any] struct {
+	// shutdownCh is closed when the server is shutting down
+	shutdownCh chan struct{}
+
+	// reqs is the request channel to the manager goroutine
+	reqs chan request[M]
+	// subs stores the subscribers
+	subs []chan M
+	// last stores the last value send to subscribers
+	last atomic.Pointer[M]
+}
+
+func (es *EventStream[M]) run() {
+	ticker := time.NewTicker(TIMEOUT)
+	defer ticker.Stop()
+
+	for req := range es.reqs {
+		switch req.cmd {
+		case SUBSCRIBE:
+			// send our last/initial value
+			req.ch <- *es.last.Load()
+			es.subs = append(es.subs, req.ch)
+		case LEAVE:
+			// find the channel that is leaving
+			for i, ch := range es.subs {
+				if ch == req.ch {
+					// swap it with the last sub and cut the slice
+					last := len(es.subs) - 1
+					es.subs[i] = es.subs[last]
+					es.subs = es.subs[:last]
+					// close the leaving channel to unblock sub
+					close(ch)
+					break
+				}
+			}
+		case SEND:
+			es.last.Store(&req.m)
+			// send to all our subs with a small timeout grace period
+			// so that clients have a bit of leeway between receives
+			ticker.Reset(TIMEOUT)
+
+			// eat a potential previous ticker value
+			select {
+			case <-ticker.C:
+			default:
+			}
+
+			for _, ch := range es.subs {
+				ticker.Reset(TIMEOUT)
+				select {
+				case ch <- req.m:
+				case <-ticker.C: // sub didn't receive fast enough
+					log.Println("event stream sub falling behind", req.m)
+					// TODO: see if we want to do book keeping and kicking of bad subs
+				}
+			}
+		case SHUTDOWN:
+			close(es.shutdownCh)
+			// after closing the above channel we shouldn't be getting anymore
+			// new subscribers so we can close all existing ones
+			for _, ch := range es.subs {
+				close(ch)
+			}
+			// and exit our background goroutine
+			return
+		}
+	}
+}
+
+func (es *EventStream[M]) Send(m M) {
+	select {
+	case es.reqs <- request[M]{cmd: SEND, m: m}:
+	case <-es.shutdownCh:
+	}
+}
+
+func (es *EventStream[M]) Sub() chan M {
+	ch := make(chan M, 8)
+
+	select {
+	case es.reqs <- request[M]{cmd: SUBSCRIBE, ch: ch}:
+	case <-es.shutdownCh:
+		close(ch) // we never subscribed so close ourselves
+	}
+	return ch
+}
+
+func (es *EventStream[M]) SubStream(ctx context.Context) Stream[M] {
+	return NewStream(ctx, es)
+}
+
+func (es *EventStream[M]) Leave(ch chan M) {
+	select {
+	case es.reqs <- request[M]{cmd: LEAVE, ch: ch}:
+	case <-es.shutdownCh:
+		// shutdown before we were able to leave, so we can assume we got closed already
+	}
+}
+
+func (es *EventStream[M]) Shutdown() {
+	select {
+	case es.reqs <- request[M]{cmd: SHUTDOWN}:
+	case <-es.shutdownCh: // someone else shut us down
+	}
+}
+
+func NewStream[T any](ctx context.Context, p *EventStream[T]) Stream[T] {
+	return &stream[T]{
+		ctx: ctx,
+		p:   p,
+		C:   p.Sub(),
+	}
+}
+
+type Stream[T any] interface {
+	Next() (T, error)
+	Close() error
+}
+
+type stream[T any] struct {
+	ctx context.Context
+	p   *EventStream[T]
+	C   chan T
+}
+
+func (s *stream[T]) Next() (v T, err error) {
+	select {
+	case v, ok := <-s.C:
+		if !ok {
+			return v, io.EOF
+		}
+		return v, nil
+	case <-s.ctx.Done():
+		return v, context.Canceled
+	}
+}
+
+func (s *stream[T]) Close() error {
+	s.p.Leave(s.C)
+	return nil
+}
---
+++ util/eventstream/eventstream_test.go
@@ -0,0 +1,138 @@
+package eventstream
+
+import (
+	"sync"
+	"testing"
+)
+
+type testCase[V any] struct {
+	name     string
+	clients  int
+	messages int
+	next     func(last V) V
+}
+
+var intCases = []testCase[int]{
+	{"only initial", 1, 0, func(last int) int { return last + 1 }},
+	{"one sub, 100 messages", 1, 100, func(last int) int { return last + 1 }},
+	{"one sub, 100000 messages", 1, 100000, func(last int) int { return last - 1 }},
+	{"100 subs, 10000 messages", 100, 10000, func(last int) int { return last + 4 }},
+}
+
+func TestEventServer(t *testing.T) {
+	for _, cas := range intCases {
+		t.Run(cas.name, func(t *testing.T) {
+			initial := 0
+			es := NewEventStream[int](initial)
+			wg := new(sync.WaitGroup)
+			// setup clients
+			client := func(ch chan int) {
+				// rename some variables for easier reasoning
+				should_next := initial
+				have_received := 0
+				should_receive := cas.messages + 1 // because of the initial value
+
+				// start receiving
+				for m := range ch {
+					have_received++
+					if m != should_next {
+						t.Error("m != next", m, should_next)
+					}
+
+					should_next = cas.next(m)
+				}
+
+				if should_receive != have_received {
+					t.Error("should_receive != have_received", should_receive, have_received)
+				}
+
+				wg.Done()
+			}
+
+			for i := 0; i < cas.clients; i++ {
+				wg.Add(1)
+				go client(es.Sub())
+			}
+
+			// send the messages
+			var val int
+			for i := 0; i < cas.messages; i++ {
+				val = cas.next(val)
+				es.Send(val)
+			}
+
+			es.Shutdown()
+			wg.Wait()
+		})
+	}
+}
+
+func TestEventServerLeave(t *testing.T) {
+	es := NewEventStream[string]("hello world")
+
+	ch := es.Sub()
+	if len(es.subs) != 1 {
+		t.Fatal("failed to subscribe")
+	}
+	es.Leave(ch)
+	es.Send("secondary")
+	if len(es.subs) != 0 {
+		t.Fatal("failed to leave:", es.subs)
+	}
+}
+
+func TestEventServerShutdown(t *testing.T) {
+	es := NewEventStream[string]("primary")
+	es.Shutdown()
+
+	ch := es.Sub()
+	if len(es.subs) != 0 {
+		t.Fatal("added a subscriber after shutdown")
+	}
+
+	es.Leave(ch)
+}
+
+var testVar string
+
+func benchmarkEventStream(subcount int, b *testing.B) {
+	es := NewEventStream("hello world")
+
+	var channels []chan string
+	for i := 0; i < subcount; i++ {
+		ch := es.Sub()
+		channels = append(channels, ch)
+		go func(ch chan string) {
+			for m := range ch {
+				testVar = m
+			}
+		}(ch)
+	}
+
+	thing := "some garbage"
+	ch := es.Sub()
+	go func() {
+		for n := 0; n < b.N; n++ {
+			es.Send(thing)
+		}
+	}()
+
+	var counter = 0
+	for range ch {
+		counter++
+		if counter >= b.N {
+			break
+		}
+	}
+	for _, ch := range channels {
+		es.Leave(ch)
+	}
+}
+
+func BenchmarkEventStream1(b *testing.B)     { benchmarkEventStream(1, b) }
+func BenchmarkEventStream5(b *testing.B)     { benchmarkEventStream(5, b) }
+func BenchmarkEventStream10(b *testing.B)    { benchmarkEventStream(10, b) }
+func BenchmarkEventStream50(b *testing.B)    { benchmarkEventStream(50, b) }
+func BenchmarkEventStream100(b *testing.B)   { benchmarkEventStream(100, b) }
+func BenchmarkEventStream1000(b *testing.B)  { benchmarkEventStream(1000, b) }
+func BenchmarkEventStream10000(b *testing.B) { benchmarkEventStream(10000, b) }
