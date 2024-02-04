a38a96f8a4acd12b0b25c2afac6c2c05d5969501
Wessie <github@wessie.info>
2024-02-04 02:22:28 +0000

    website: add queue data

--- website/public/queue.go
+++ website/public/queue.go
@@ -1,20 +1,36 @@
 package public
 
 import (
-	"log"
 	"net/http"
+
+	radio "github.com/R-a-dio/valkyrie"
 )
 
-func (s State) GetQueue(w http.ResponseWriter, r *http.Request) {
-	queueInput := struct {
+func (s State) getQueue(w http.ResponseWriter, r *http.Request) error {
+	input := struct {
 		shared
+		Queue []radio.QueueEntry
 	}{
 		shared: s.shared(r),
 	}
 
-	err := s.TemplateExecutor.ExecuteFull(theme, "queue", w, queueInput)
+	queue, err := s.Streamer.Queue(r.Context())
 	if err != nil {
-		log.Println(err)
+		return err
+	}
+	input.Queue = queue
+
+	err = s.TemplateExecutor.ExecuteFull(theme, "queue", w, input)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s State) GetQueue(w http.ResponseWriter, r *http.Request) {
+	err := s.getQueue(w, r)
+	if err != nil {
+		s.errorHandler(w, r, err)
 		return
 	}
 }
