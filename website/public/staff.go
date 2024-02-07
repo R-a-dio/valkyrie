2737bab6cd9387d0fb6f62b94cd28f1bc705acb9
Wessie <github@wessie.info>
2024-02-07 16:09:04 +0000

    website: fix staff page data input

--- website/public/staff.go
+++ website/public/staff.go
@@ -2,16 +2,30 @@ package public
 
 import (
 	"net/http"
+
+	radio "github.com/R-a-dio/valkyrie"
 )
 
 type StaffInput struct {
 	SharedInput
+
+	Users []radio.User
 }
 
-func NewStaffInput(r *http.Request) StaffInput {
-	return StaffInput{
-		SharedInput: NewSharedInput(r),
+func (si StaffInput) Roles() []string {
+	return []string{"staff", "dev", "djs"}
+}
+
+func NewStaffInput(us radio.UserStorageService, r *http.Request) (*StaffInput, error) {
+	users, err := us.User(r.Context()).All()
+	if err != nil {
+		return nil, err
 	}
+
+	return &StaffInput{
+		SharedInput: NewSharedInput(r),
+		Users:       users,
+	}, nil
 }
 
 func (StaffInput) TemplateBundle() string {
@@ -19,9 +33,13 @@ func (StaffInput) TemplateBundle() string {
 }
 
 func (s State) GetStaff(w http.ResponseWriter, r *http.Request) {
-	input := NewStaffInput(r)
+	input, err := NewStaffInput(s.Storage, r)
+	if err != nil {
+		s.errorHandler(w, r, err)
+		return
+	}
 
-	err := s.Templates.Execute(w, r, input)
+	err = s.Templates.Execute(w, r, input)
 	if err != nil {
 		s.errorHandler(w, r, err)
 		return
