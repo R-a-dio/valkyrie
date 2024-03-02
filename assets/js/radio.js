9daab1cef35dc4b1769a7e7dd6fe4991f906c84f
Wessie <github@wessie.info>
2024-03-02 01:34:53 +0000

    website/js: try and fix the audio.src nulling generating errors

--- assets/js/radio.js
+++ assets/js/radio.js
@@ -294,7 +294,8 @@ class Stream {
         if (deleteAudio) {
             // due to eager buffering by browsers they will keep the stream 'playing'
             // in the background if we don't do the below things.
-            this.audio.src = null;
+            this.audio.src = "";
+            this.audio.removeAttribute("src");
             this.audio.load();
             this.audio = null;
         }
