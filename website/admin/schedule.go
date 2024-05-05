442b7e606db035dbbc240b4c1f8268c67e1bcb6d
Wessie <github@wessie.info>
2024-05-05 16:57:41 +0100

    website/admin: add available users (that are dj) to the schedule input

--- website/admin/schedule.go
+++ website/admin/schedule.go
@@ -3,6 +3,7 @@ package admin
 import (
 	"html/template"
 	"net/http"
+	"slices"
 
 	radio "github.com/R-a-dio/valkyrie"
 	"github.com/R-a-dio/valkyrie/errors"
@@ -14,29 +15,46 @@ type ScheduleInput struct {
 	middleware.Input
 	CSRFTokenInput template.HTML
 
-	Schedule []*radio.ScheduleEntry
+	Schedule     []*radio.ScheduleEntry
+	AvailableDJs []radio.User
 }
 
 func (ScheduleInput) TemplateBundle() string {
 	return "schedule"
 }
 
-func NewScheduleInput(ss radio.ScheduleStorage, r *http.Request) (*ScheduleInput, error) {
+func NewScheduleInput(ss radio.ScheduleStorage, us radio.UserStorage, r *http.Request) (*ScheduleInput, error) {
 	const op errors.Op = "website/admin.NewScheduleInput"
 	schedule, err := ss.Latest()
 	if err != nil {
 		return nil, errors.E(op, err)
 	}
 
+	users, err := us.All()
+	if err != nil {
+		return nil, errors.E(op, err)
+	}
+	// filter users down to just djs
+	users = slices.DeleteFunc(users, func(u radio.User) bool {
+		return u.UserPermissions.Has(radio.PermDJ)
+	})
+
 	return &ScheduleInput{
 		Input:          middleware.InputFromRequest(r),
 		CSRFTokenInput: csrf.TemplateField(r),
 		Schedule:       schedule,
+		AvailableDJs:   users,
 	}, nil
 }
 
 func (s *State) GetSchedule(w http.ResponseWriter, r *http.Request) {
-	input, err := NewScheduleInput(s.Storage.Schedule(r.Context()), r)
+	ctx := r.Context()
+
+	input, err := NewScheduleInput(
+		s.Storage.Schedule(ctx),
+		s.Storage.User(ctx),
+		r,
+	)
 	if err != nil {
 		s.errorHandler(w, r, err, "")
 		return
