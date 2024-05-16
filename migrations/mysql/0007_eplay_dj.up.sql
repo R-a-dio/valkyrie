6b622b8ecf31105839a5ed2e77c059ba8c50228d
Wessie <github@wessie.info>
2024-05-16 22:30:43 +0100

    migrations: fix slow migrations

--- migrations/mysql/0007_eplay_dj.up.sql
+++ migrations/mysql/0007_eplay_dj.up.sql
@@ -1,3 +1,3 @@
 ALTER TABLE eplay ADD COLUMN IF NOT EXISTS (djs_id INT DEFAULT NULL);
 
-UPDATE eplay SET djs_id = (SELECT dj FROM listenlog WHERE eplay.dt >= listenlog.time ORDER BY listenlog.time ASC LIMIT 1);
+update eplay set djs_id = (SELECT dj FROM listenlog WHERE eplay.dt >= listenlog.time ORDER BY listenlog.time ASC LIMIT 1) where eplay.dt > (SELECT time FROM listenlog ORDER BY time ASC limit 1);
