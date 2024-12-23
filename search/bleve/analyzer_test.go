9cdfe27f7966bbca1e84fb990e4609fe04b50282
Wessie <github@wessie.info>
2024-12-23 19:17:55 +0100

    search/bleve: add some tests, benchmarks and fuzzer

---
+++ search/bleve/analyzer_test.go
@@ -0,0 +1,116 @@
+package bleve
+
+import (
+	"strings"
+	"testing"
+	"time"
+	"unicode/utf8"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func FuzzTokenizer(f *testing.F) {
+	tok := NewKagomeTokenizer()
+
+	f.Fuzz(func(t *testing.T, in []byte) {
+		now := time.Now()
+		out := tok.Tokenize(in)
+		taken := time.Since(now)
+		t.Log(taken)
+
+		for _, token := range out {
+			if !assert.Equal(t, token.Term, in[token.Start:token.End]) {
+				t.Log(in)
+				t.Log(token.Term)
+				t.Log(token)
+				t.Fail()
+			}
+		}
+
+		if taken > time.Second*1 {
+			t.Fatal("took too long", taken, in)
+		}
+	})
+}
+
+var benches = []struct {
+	name  string
+	input string
+}{
+	{"rune-small", "桃色技術音楽堂"},
+	{"rune-medium", "feat. Hanamori Rie Pink smoothie 桃色技術音楽堂×中路もとめ"},
+	{"rune-worst-case", repeatToLength("桃色技術音楽堂", MaxQuerySize)},
+	{"ascii-small", "Hikasa Youko"},
+	{"ascii-medium", "Toyosaki Aki & Hikasa Youko & Satou Satomi & Kotobuki Minako & Taketatsu Ayana U&I (Studio Mix)"},
+	{"ascii-worst-case", repeatToLength("Toyosaki Aki & Hikasa Youko & Satou Satomi & Kotobuki Minako & Taketatsu Ayana U&I (Studio Mix)", MaxQuerySize)},
+}
+
+func repeatToLength(s string, l int) string {
+	return CutoffAtRune(strings.Repeat(s, l/len(s)+1)[:l])
+}
+
+func BenchmarkAnalyzer(b *testing.B) {
+	idx := newIndex(b)
+	a := idx.index.Mapping().AnalyzerNamed(indexAnalyzerName)
+
+	fn := func(s string) func(b *testing.B) {
+		in := []byte(s)
+		return func(b *testing.B) {
+			for range b.N {
+				a.Analyze(in)
+			}
+		}
+	}
+
+	for _, bench := range benches {
+		b.Run(bench.name, fn(bench.input))
+	}
+}
+
+func BenchmarkTokenizer(b *testing.B) {
+	tok := NewKagomeTokenizer()
+
+	fn := func(s string) func(b *testing.B) {
+		in := []byte(s)
+		return func(b *testing.B) {
+			for range b.N {
+				tok.Tokenize(in)
+			}
+		}
+	}
+
+	for _, bench := range benches {
+		b.Run(bench.name, fn(bench.input))
+	}
+}
+
+func BenchmarkIsASCII(b *testing.B) {
+	fn := func(s string) func(b *testing.B) {
+		in := []byte(s)
+		return func(b *testing.B) {
+			for range b.N {
+				IsASCII(in)
+			}
+		}
+	}
+
+	for _, bench := range benches {
+		b.Run(bench.name, fn(bench.input))
+	}
+}
+
+func BenchmarkCutoffWorstCase(b *testing.B) {
+	// this benchmark just tests our worst case if someone crafted a string specifically to
+	// hit the RuneError path all the way to nothing
+	var i []rune
+	for range MaxQuerySize {
+		i = append(i, utf8.RuneError)
+	}
+	in := string(i)
+	assert.Len(b, CutoffAtRune(in), 0)
+
+	b.ResetTimer()
+	for range b.N {
+		CutoffAtRune(in)
+	}
+}
---
+++ search/bleve/index_test.go
@@ -0,0 +1,86 @@
+package bleve
+
+import (
+	"context"
+	"testing"
+
+	radio "github.com/R-a-dio/valkyrie"
+	"github.com/blevesearch/bleve/v2"
+	"github.com/stretchr/testify/require"
+)
+
+func newIndex(t testing.TB) *indexWrap {
+	idx, err := NewIndex(":memory:")
+	require.NoError(t, err)
+	t.Cleanup(func() {
+		idx.Close()
+	})
+	return idx
+}
+
+type song struct {
+	artist, title string
+}
+
+var testData = []song{
+	{"桃色技術音楽堂×中路もとめ feat. Hanamori Rie", "Pink smoothie"},
+	{"School Food Punishment", "How To Go"},
+	{"Taishi feat. みとせのりこ", "Personalizer"},
+	{"Toyosaki Aki & Hikasa Youko & Satou Satomi & Kotobuki Minako & Taketatsu Ayana", "U&I (Studio Mix)"},
+}
+
+func TestIndexing(t *testing.T) {
+	ctx := context.Background()
+	idx := newIndex(t)
+
+	var songs []radio.Song
+	for i, song := range testData {
+		songs = append(songs, radio.Song{
+			DatabaseTrack: &radio.DatabaseTrack{
+				TrackID: radio.TrackID(i),
+				Title:   song.title,
+				Artist:  song.artist,
+			},
+		})
+	}
+
+	// add the songs to the index
+	err := idx.Index(ctx, songs)
+	require.NoError(t, err)
+
+	// make sure they now exist
+	count, err := idx.index.DocCount()
+	require.NoError(t, err)
+	require.EqualValues(t, len(testData), count)
+
+	// now do our search tests
+	rq, err := NewQuery(ctx, "motome hana")
+	require.NoError(t, err)
+
+	req := bleve.NewSearchRequestOptions(rq, 100, 0, true)
+	req.SortBy(DefaultSort)
+	req.Fields = dataField
+
+	res, err := idx.index.Search(req)
+	require.NoError(t, err)
+
+	t.Log(res)
+}
+
+func TestAnalyzer(t *testing.T) {
+	idx := newIndex(t)
+
+	ian := idx.index.Mapping().AnalyzerNamed(indexAnalyzerName)
+
+	for _, in := range testData {
+		in := radio.Metadata(in.artist, in.title)
+		ires := ian.Analyze([]byte(in))
+
+		t.Log("============", in, "============")
+		t.Log("============ INDEX ============")
+		for i, token := range ires {
+			t.Logf("%d %s\n", i, token)
+		}
+		t.Log("===============================")
+	}
+}
