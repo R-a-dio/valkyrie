f695139f0401996bd41adb1866acacd5e25553c2
Wessie <github@wessie.info>
2024-12-27 20:25:46 +0100

    assets/js: adjust prettyDuration to be shorter

--- assets/js/radio.js
+++ assets/js/radio.js
@@ -107,12 +107,12 @@ htmx.on('htmx:afterSettle', (event) => {
 function prettyDuration(d) {
     if (d < 0) {
         if (d > -60) {
-            return "less than a minute ago"
+            return "<1 minute ago"
         }
         return rtf.format(Math.floor(d / 60), "minute")
     }
     if (d < 60) {
-        return "in less than a minute"
+        return "in <1 minute"
     }
     return rtf.format(Math.floor(d / 60), "minute");
 }
