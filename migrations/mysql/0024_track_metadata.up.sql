98256e4512a33e6bd7350e717145edb0509747c9
kipukun <kipukun@waifu.club>
2025-02-11 23:57:59 -0500

    storage: fixed length primary keys for track_metadata

--- migrations/mysql/0024_track_metadata.up.sql
+++ migrations/mysql/0024_track_metadata.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `track_metadata` (
     `track_id` int(14) unsigned NOT NULL,
-    `id` text(36) NOT NULL,
-    `provider` text NOT NULL,
+    `id` varchar(36) NOT NULL,
+    `provider` varchar(255) NOT NULL,
     `album_art_path` text NOT NULL,
     `primary` bit NOT NULL,
     `safe` bit NOT NULL,
