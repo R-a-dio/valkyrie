e0afe3e9f4deec9438698c38c3ee58b22c27f395
Wessie <github@wessie.info>
2025-02-12 16:56:46 +0100

    util/buildinfo: add modified and modification time to buildinfo vars

--- util/buildinfo/buildinfo.go
+++ util/buildinfo/buildinfo.go
@@ -4,21 +4,28 @@ import (
 	"runtime/debug"
 )
 
+const (
+	keyRevision = "vcs.revision"
+	keyTime     = "vcs.time"
+	keyModified = "vcs.modified"
+)
+
 var (
+	GitRef                 = getBuildInfoKey(keyRevision, "(devel)")
+	GitMod                 = getBuildInfoKey(keyTime, "unknown")
 	InstrumentationName    = "github.com/R-a-dio/valkyrie"
-	InstrumentationVersion = commitHash()
-	Version                = commitHash()
-	GitRef                 = commitHash()
+	InstrumentationVersion = GitRef
+	Version                = GitRef
 	ShortRef               = GitRef[:7]
 )
 
-func commitHash() string {
-	if info, ok := debug.ReadBuildInfo(); ok { // requires go version 1.12+
+func getBuildInfoKey(key string, def string) string {
+	if info, ok := debug.ReadBuildInfo(); ok {
 		for _, setting := range info.Settings {
-			if setting.Key == "vcs.revision" {
+			if setting.Key == key {
 				return setting.Value
 			}
 		}
 	}
-	return "(devel)"
+	return def
 }
