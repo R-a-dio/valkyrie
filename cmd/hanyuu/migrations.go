e8e343e5f346cb29bd8a16e888c9895cc55b6c00
Wessie <github@wessie.info>
2025-02-15 23:25:58 +0100

    cmd/hanyuu: replace cli with cobra-based one

--- cmd/hanyuu/migrations.go
+++ cmd/hanyuu/migrations.go
@@ -2,112 +2,82 @@ package main
 
 import (
 	"context"
-	"flag"
-	"fmt"
-	"log"
 	"os"
-	"path"
 	"strconv"
 
-	"github.com/R-a-dio/valkyrie/config"
 	"github.com/R-a-dio/valkyrie/errors"
 	"github.com/R-a-dio/valkyrie/migrations"
-	"github.com/rs/zerolog"
+	"github.com/spf13/cobra"
 
 	"github.com/golang-migrate/migrate/v4"
-	"github.com/golang-migrate/migrate/v4/source"
-	"github.com/google/subcommands"
 )
 
-type migrateCmd struct {
-	// command line flags
-	flags   *flag.FlagSet
-	verbose bool
+func MigrationCommand() *cobra.Command {
+	root := &cobra.Command{
+		Use:     "migrate",
+		GroupID: "helpers",
+		Short:   "tool to manage migrations against the database",
+	}
 
-	// migration drivers
-	source  source.Driver
-	migrate *migrate.Migrate
+	root.AddCommand(
+		&cobra.Command{
+			Use:   "version",
+			Short: "return the current schema version",
+			Args:  cobra.NoArgs,
+			RunE:  SimpleCommand(MigrationVersion),
+		},
+		&cobra.Command{
+			Use:   "up",
+			Short: "migrate the schema version up",
+			Args:  cobra.NoArgs,
+			RunE:  SimpleCommand(MigrationUp),
+		},
+		&cobra.Command{
+			Use:   "force <version>",
+			Short: "force the schema version, this does not run any migrations",
+			RunE:  SimpleCommand(MigrationForceVersion),
+			Args:  cobra.ExactArgs(1),
+			ValidArgsFunction: func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+				source, err := migrations.NewSource(cmd.Context(), cfgFromContext(cmd.Context()))
+				if err != nil {
+					return nil, cobra.ShellCompDirectiveNoFileComp
+				}
+
+				var things []string
+				for {
+					version, err := source.First()
+					if err != nil {
+						if errors.IsE(err, os.ErrNotExist) {
+							break
+						}
+						return nil, cobra.ShellCompDirectiveError | cobra.ShellCompDirectiveNoFileComp
+					}
+
+					things = append(things, strconv.Itoa(int(version)))
+				}
+
+				return things, cobra.ShellCompDirectiveNoFileComp
+			},
+		},
+		&cobra.Command{
+			Use:   "is-latest",
+			Short: "checks if you're on the latest version of the schema",
+			Args:  cobra.NoArgs,
+			RunE:  Command(MigrationIsLatestVersion),
+		},
+		&cobra.Command{
+			Use:   "ls",
+			Short: "lists all available migrations",
+			Args:  cobra.NoArgs,
+			RunE:  SimpleCommand(MigrationList),
+		},
+	)
+
+	return root
 }
 
-func (m migrateCmd) Name() string {
-	return "migrate"
-}
-
-func (m migrateCmd) Synopsis() string {
-	return "migrate allows you to handle database migrations"
-}
-
-func (m migrateCmd) Usage() string {
-	return "migrate"
-}
-
-func (m *migrateCmd) SetFlags(f *flag.FlagSet) {
-	f.BoolVar(&m.verbose, "verbose", false, "verbose output")
-}
-
-func (m *migrateCmd) Execute(ctx context.Context, f *flag.FlagSet, args ...any) subcommands.ExitStatus {
-	m.flags = f
-	defer func() {
-		if m.migrate != nil {
-			m.migrate.Close()
-		}
-	}()
-
-	cmder := subcommands.NewCommander(f, path.Base(os.Args[0])+" migrate")
-	cmder.Register(cmder.HelpCommand(), "")
-	cmder.Register(cmder.CommandsCommand(), "")
-	cmder.Register(cmd{
-		name:     "version",
-		synopsis: "return the current schema version",
-		usage: `version:
-		return the current schema version
-		`,
-		execute: withConfig(m.version),
-	}, "")
-	cmder.Register(cmd{
-		name:     "up",
-		synopsis: "migrate the schema version up",
-		usage: `up:
-		migrate the schema version up
-		`,
-		execute: withConfig(m.up),
-	}, "")
-	cmder.Register(cmd{
-		name:     "force",
-		synopsis: "set the current version of the schema, this does not run any migrations",
-		usage: `force <version>:
-		set the current version of the schema, this does not run any migrations but only
-		records what version the schema currently is
-		`,
-		execute: withConfig(m.forceVersion),
-	}, "")
-	cmder.Register(cmd{
-		name:     "ls",
-		synopsis: "shows a list of all migrations",
-		usage: `ls:
-		shows a list of all migrations
-		`,
-		execute: withConfig(m.ls),
-	}, "")
-	cmder.Register(cmd{
-		name:     "is-latest",
-		synopsis: "checks if you're on the latest version",
-		usage: `is-latest:
-		checks if the migrations have all been ran and we are on the latest version
-		if we're not we exit with a non-zero exit code
-		`,
-		execute: withConfig(m.isLatest),
-	}, "")
-
-	zerolog.Ctx(ctx).UpdateContext(func(zc zerolog.Context) zerolog.Context {
-		return zc.Str("service", "migrate")
-	})
-
-	return cmder.Execute(ctx, args...)
-}
-
-func (m migrateCmd) up(ctx context.Context, cfg config.Config) error {
-	err := m.setup(ctx, cfg)
+func MigrationUp(cmd *cobra.Command, args []string) error {
+	m, err := MigrationSetup(cmd)
 	if err != nil {
 		return err
 	}
@@ -115,48 +85,48 @@ func (m migrateCmd) up(ctx context.Context, cfg config.Config) error {
 	done := make(chan struct{})
 	defer close(done)
 
-	go func() {
+	go func(ctx context.Context) {
 		select {
 		case <-ctx.Done():
-			m.migrate.GracefulStop <- true
+			m.GracefulStop <- true
 		case <-done:
 		}
-	}()
+	}(cmd.Context())
 
-	return m.migrate.Up()
+	return m.Up()
 }
 
-func (m migrateCmd) ls(ctx context.Context, cfg config.Config) error {
-	err := m.setup(ctx, cfg)
+func MigrationList(cmd *cobra.Command, args []string) error {
+	source, err := migrations.NewSource(cmd.Context(), cfgFromContext(cmd.Context()))
 	if err != nil {
 		return err
 	}
 
-	version, err := m.source.First()
+	version, err := source.First()
 	if err != nil {
 		return err
 	}
 
 	for {
-		_, identifier, err := m.source.ReadUp(version)
+		_, identifier, err := source.ReadUp(version)
 		if err != nil && !errors.IsE(err, os.ErrNotExist) {
 			return err
 		}
 
 		if !errors.IsE(err, os.ErrNotExist) {
-			fmt.Printf("%.4d   UP %s\n", version, identifier)
+			cmd.Printf("%.4d   UP %s\n", version, identifier)
 		}
 
-		_, identifier, err = m.source.ReadDown(version)
+		_, identifier, err = source.ReadDown(version)
 		if err != nil && !errors.IsE(err, os.ErrNotExist) {
 			return err
 		}
 
 		if !errors.IsE(err, os.ErrNotExist) {
-			fmt.Printf("%.4d DOWN %s\n", version, identifier)
+			cmd.Printf("%.4d DOWN %s\n", version, identifier)
 		}
 
-		version, err = m.source.Next(version)
+		version, err = source.Next(version)
 		if err != nil {
 			if errors.IsE(err, os.ErrNotExist) {
 				return nil
@@ -166,15 +136,10 @@ func (m migrateCmd) ls(ctx context.Context, cfg config.Config) error {
 	}
 }
 
-func (m migrateCmd) forceVersion(ctx context.Context, cfg config.Config) error {
-	err := m.setup(ctx, cfg)
+func MigrationForceVersion(cmd *cobra.Command, args []string) error {
+	m, err := MigrationSetup(cmd)
 	if err != nil {
 		return err
-	}
-
-	args := m.flags.Args()
-	if len(args) < 2 {
-		return errors.New("missing version argument")
 	}
 
 	version, err := strconv.Atoi(args[1])
@@ -182,20 +147,18 @@ func (m migrateCmd) forceVersion(ctx context.Context, cfg config.Config) error {
 		return errors.Errorf("malformed version: %s", err)
 	}
 
-	return m.migrate.Force(version)
+	return m.Force(version)
 }
 
-func (m migrateCmd) isLatest(ctx context.Context, cfg config.Config) error {
-	return migrations.CheckVersion(ctx, cfg)
-}
+var MigrationIsLatestVersion = migrations.CheckVersion
 
-func (m migrateCmd) version(ctx context.Context, cfg config.Config) error {
-	err := m.setup(ctx, cfg)
+func MigrationVersion(cmd *cobra.Command, args []string) error {
+	m, err := MigrationSetup(cmd)
 	if err != nil {
 		return err
 	}
 
-	v, d, err := m.migrate.Version()
+	v, d, err := m.Version()
 	if err != nil {
 		return err
 	}
@@ -205,24 +168,28 @@ func (m migrateCmd) version(ctx context.Context, cfg config.Config) error {
 		state = "dirty"
 	}
 
-	fmt.Printf("version: %d, state: %s\n", v, state)
+	cmd.Printf("version: %d, state: %s\n", v, state)
 	return nil
 }
 
-func (m *migrateCmd) setup(ctx context.Context, cfg config.Config) error {
-	migr, err := migrations.New(ctx, cfg)
+func MigrationSetup(cmd *cobra.Command) (*migrate.Migrate, error) {
+	migr, err := migrations.New(cmd.Context(), cfgFromContext(cmd.Context()))
 	if err != nil {
-		return err
+		return nil, err
 	}
-	if m.verbose {
-		migr.Log = migrateLog{log.New(os.Stderr, "", log.LstdFlags)}
-	}
-	m.migrate = migr
-	return nil
+
+	migr.Log = migrateLog{cmd}
+	return migr, nil
 }
 
 type migrateLog struct {
-	*log.Logger
+	l interface {
+		Printf(format string, args ...any)
+	}
+}
+
+func (ml migrateLog) Printf(format string, args ...any) {
+	ml.l.Printf(format, args...)
 }
 
 func (ml migrateLog) Verbose() bool {
